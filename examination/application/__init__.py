69b254066b8c5bb701d864fa8641f169eb170564
Issam Hariri <issamhariri@outlook.com>
2025-10-21 10:50:54 +0200

    Denna fil startar innan app.py den initierar programmet och är motorn i programmet, all logik finns sedan i app filen

--- examination/application/__init__.py
+++ examination/application/__init__.py
@@ -5,3 +5,10 @@ I filen skapar jag en funktion som heter create_app()
 Flask känner igen funktionen och startar den.
 Genom att ha en separat app.py fil och init.py fil så skapas en plats för
 logik samt en plats för initiering."""
+from flask import Flask
+from .app import routes
+
+def create_app():
+    app = Flask(__name__)
+    routes(app)
+    return app
