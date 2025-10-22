2f91c596589c5f83da6842acaf783f20555e0b4b
Issam Hariri <issamhariri@outlook.com>
2025-10-22 11:57:01 +0200

    uppdaterat test fil som är i linje med mitt nya projekt

--- examination/tests/test_api_client.py
+++ examination/tests/test_api_client.py
@@ -1,5 +1,6 @@
 #Denna fil ska testa API klienten
 #Tester som exempel:
-"""Ett giltigt API svar ska ge 24 poster(timmar),
-   404 svar ska hanteras utan krasch,
-  Timeout ska hanteras utan krasch"""
+"""# Test: hämtar data för flera tickers -> en rad per ticker
+# Test: API 404 -> hanteras utan krasch
+# Test: timeout -> hanteras utan krasch
+"""
