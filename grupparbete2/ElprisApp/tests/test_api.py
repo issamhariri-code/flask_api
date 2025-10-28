4446a93648c21ebe6bc343a84461c2780d3fb80f
Issam Hariri <issamhariri@outlook.com>
2025-10-28 23:06:11 +0100

    grupparbete examinationsuppgift

--- grupparbete2/ElprisApp/tests/test_api.py
+++ grupparbete2/ElprisApp/tests/test_api.py
@@ -1,5 +1,49 @@
+"""
+tester för utils.api_handler.get_elpriser
+
+tester:
+testa att funktionen går att anropa utan fel
+resultat ska vara av rätt typ
+testa att datan om den finns, har rätt struktur.
+
+"""
+
 from utils.api_handler import get_elpriser
 
 def test_get_elpriser_returns_data():
+    """
+    testar att funktionen går att anropa och returnerar något
+    """
     data = get_elpriser(2025, 10, 26, "SE3")
     assert data is not None
+
+
+def test_get_elpriser_returns_list():
+    """
+    funktionen ska returnera en lista (även om tom).
+    """
+    data = get_elpriser(2025, 10, 26, "SE3")
+
+    assert isinstance(data, list) 
+    #Funktionen ska returnera en lista
+
+def test_get_elpriser_item_structure():
+    """
+    Om listan inte är tom ska varje rad vara en dict med nycklarna:
+    time_start och SEK_per_kWh
+    """
+    data = get_elpriser(2025, 10, 26, "SE3")
+
+    if len(data) > 0: #kollar om listan data innehåller minst ett element
+        first = data[0] #hämtar data från index [0] och sparar i variabeln
+        assert isinstance(first, dict) #kontrollerar att första element är en dict 
+#om inte misslyckas test med ett meddelande om att varje rad ska vara en dict
+        assert "time_start" in first #Nyckeln 'time_start' saknas
+        assert "SEK_per_kWh" in first #Nyckeln 'SEK_per_kWh' saknas
+
+
+
+
+
+
+
--- grupparbete2/ElprisApp/tests/test_data.py
+++ grupparbete2/ElprisApp/tests/test_data.py
@@ -1,6 +1,50 @@
+"""
+tester för utils.data_tools.create_dataframe
+
+testa att funktionen returnerar dataframe och testar att
+kolumnerna finns
+testa att felaktig input hanteras
+
+"""
+
+
+import pytest
+import pandas as pd
 from utils.data_tools import create_dataframe
 
 def test_create_dataframe():
     sample = [{"time_start":"00:00","SEK_per_kWh":0.5}]
     df = create_dataframe(sample)
     assert "time_start" in df.columns and "SEK_per_kWh" in df.columns
+
+
+
+def test_create_dataframe_handles_empty_list():
+    # tom lista skickas in
+    df = create_dataframe([])
+    # df innehåller det som funktionen returnerar
+    # i detta fall en pandas dataframe 
+
+    # det ska fortfarande bli en dataframe
+    assert type(df) == pd.DataFrame
+    # funktionen kan hantera tom input
+    # tom lista blir 0 rader 
+    assert len(df) == 0
+
+    # Kolumnerna måste finnas
+    assert "time_start" in df.columns
+    assert "SEK_per_kWh" in df.columns
+
+
+def test_create_dataframe_invalid_input():
+    """
+    Enkel variant av feltest: vi anropar funktionen med None
+    och kontrollerar att funktionen kastar ett fel.
+    """
+
+    try:
+        create_dataframe(None)
+        assert False, "Funktionen skulle kasta fel vid felaktig input"
+    except:
+        # Om vi hamnar här fungerar testet
+        assert True
