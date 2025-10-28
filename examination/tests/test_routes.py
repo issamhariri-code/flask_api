a550d26126a72e0d89638b513b2828178377de47
Issam Hariri <issamhariri@outlook.com>
2025-10-28 16:06:29 +0100

    lagt in docstrings för testfil

--- examination/tests/test_routes.py
+++ examination/tests/test_routes.py
@@ -1,6 +1,31 @@
-#Denna fil ska testa Flask routes
-#Exempeltester
-"""GET / ska visa formuläret,
-   POST med giltig data visar resultat,
-   POST med ogiltig data visar fel,
- Okänd adress ska ge egen skapad 404 sida"""
+"""
+GET 
+ska svara 200
+ska innehålla formulärets fält (tickers, datum ,annat)
+använder index.html template
+
+POST - ogiltig input
+om datum saknas - visar felmeddelande på index.html
+ingen ticker angett - visar felmeddelande
+för många tickers - visar fel
+datum i framtiden - visar fel
+vid fel ska inte sidan krascha utan gå igenom index igen
+
+
+POST - giltig input
+anropar validation fil - ok
+anropar api_client fil
+renderar result.html template
+result.html innehåller 
+(datum, perspektiv, tabellhuvud, rader för varje ticker med data)
+
+
+POST - varning från api_client
+om ticker saknar data - varning syns i result.html 
+övriga tickers visas normalt
+
+output:
+response status
+rätt template används
+rätt text i HTML
+"""
